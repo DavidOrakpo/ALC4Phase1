9f826ab5c69f832fcd6950c306ba204e3a4bc07e
David Orakpo <davidorakpo65@gmail.com>
2019-08-30 01:57:51 +0100

    Changed some constraint files

--- app/src/main/java/com/example/alc4phase1/MyProfile.java
+++ app/src/main/java/com/example/alc4phase1/MyProfile.java
@@ -9,6 +9,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -20,6 +21,13 @@ public class MyProfile extends AppCompatActivity {
         setContentView(R.layout.activity_my_profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle("Title");
+        }
+
 
         ImageButton imageButton = findViewById(R.id.imageButton);
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +41,15 @@ public class MyProfile extends AppCompatActivity {
         view.setBackgroundColor(getResources().getColor(R.color.yellow));
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     public void toHomepage(){
         Intent intent = new Intent(this,MainActivity.class);
